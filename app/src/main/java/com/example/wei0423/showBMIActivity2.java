3b1f1fbfb91909c63248dd2e133f389bd3bf6e23
Wei0423 <107B20263@mailst.cjcu.edu.tw>
2020-04-24 11:11:33 +0800

    更改把英文變成中文

--- app/src/main/java/com/example/wei0423/showBMIActivity2.java
+++ app/src/main/java/com/example/wei0423/showBMIActivity2.java
@@ -35,15 +35,15 @@ public class showBMIActivity2 extends AppCompatActivity {
         String rem;
         if (bmi<18.5){
             rem="體重過輕";
-           Toast.makeText(this, "體重過輕", Toast.LENGTH_LONG).show();
+           Toast.makeText(this, R.string.strtoolight, Toast.LENGTH_LONG).show();
             view1.setImageResource(R.drawable.bmi1);
         }else if(bmi>24){
             rem="體重過重";
-          Toast.makeText(this, "體重過重", Toast.LENGTH_LONG).show();
+          Toast.makeText(this, R.string.strheavy, Toast.LENGTH_LONG).show();
             view1.setImageResource(R.drawable.bmi3);
         }else{
             rem="正常體重";
-          Toast.makeText(this, "正常體重", Toast.LENGTH_LONG).show();
+          Toast.makeText(this, R.string.strnormal, Toast.LENGTH_LONG).show();
             view1.setImageResource(R.drawable.bmi2);
         }
      Toast.makeText(this,rem, Toast.LENGTH_LONG).show();
